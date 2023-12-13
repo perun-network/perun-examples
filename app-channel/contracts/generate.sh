a67fcf7370c44cd4c89e3f07cb78e335421e7265
Minh Huy Tran <huy.tranminh2804@gmail.com>
2023-12-13 13:08:21 +0100

    update contract to solidity 0.8.0 and generate new bindings

--- app-channel/contracts/generate.sh
+++ app-channel/contracts/generate.sh
@@ -2,28 +2,46 @@
 
 set -e
 
-if [ -z "$ABIGEN" ]
+# Define ABIGEN and SOLC default values.
+ABIGEN=abigen
+SOLC=solc
+
+echo 'Please ensure that solc v0.8.15+ and abigen 1.10.18+ are installed.'
+
+if ! $ABIGEN --version
 then
-    ABIGEN=abigen
+    echo "'abigen' not found. Please add to PATH or set ABIGEN='path_to_abigen'."
+    exit 1
 fi
 
-if [ -z "$SOLC" ]
+if ! $SOLC --version
 then
-    SOLC=solc
+    echo "'solc' not found. Please add to PATH or set SOLC='path_to_solc'."
+    exit 1
 fi
+
+echo "Please ensure that the repository was cloned with submodules: 'git submodule update --init --recursive'."
 
 # Generate golang bindings from solidity contract
 # Argument 1: solidity contract file
 # Argument 2: golang contract name (used for package and file)
 generate_bindings() {
-    CONTRACT_SOL_FILE=$1
-    CONTRACT_GO_NAME=$2
-    PKG=$CONTRACT_GO_NAME
+    FILE=$1
+    PKG=$2
+    CONTRACT=$FILE
     GENDIR=./generated/$PKG
+
+    echo "Generating $PKG bindings..."
+    rm -r $GENDIR
     mkdir -p $GENDIR
-    $ABIGEN --pkg $PKG --sol $CONTRACT_SOL_FILE --out $GENDIR/$CONTRACT_GO_NAME.go --solc $SOLC
+
+    # Compile and generate binary runtime
+    $SOLC --abi --bin --optimize $FILE.sol -o $GENDIR
+
+    # Generate bindings.
+    $ABIGEN --pkg $PKG --abi $GENDIR/$FILE.abi --bin $GENDIR/$FILE.bin --out $GENDIR/$CONTRACT.go
 }
 
-generate_bindings ./TicTacToeApp.sol ticTacToeApp
+generate_bindings "TicTacToeApp" "ticTacToeApp"
 # generate_bindings ./perun-eth-contracts/contracts/Adjudicator.sol adjudicator
 # generate_bindings ./perun-eth-contracts/contracts/AssetHolderETH.sol assetHolderETH
